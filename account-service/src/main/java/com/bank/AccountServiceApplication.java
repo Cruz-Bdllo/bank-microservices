16315a010fd3de44239d1149a202cfc2145241d2
Juan Manuel Cruz Badillo <cubj25@gmail.com>
2023-02-20 21:41:50 -0600

    feat(accounts): Generate POM dependencies for account service

---
+++ account-service/src/main/java/com/bank/AccountServiceApplication.java
@@ -0,0 +1,17 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class AccountServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AccountServiceApplication.class);
+    }
+}
