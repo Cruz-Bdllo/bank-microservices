efd6d1a6a852bc76d4bc53c3996228576e6802b2
Juan Manuel Cruz Badillo <cubj25@gmail.com>
2023-03-07 00:43:38 -0600

    feat(loans): Add registry config for add Eureka server

--- loan-service/src/main/java/com/bank/loan/LoanServiceApplication.java
+++ loan-service/src/main/java/com/bank/loan/LoanServiceApplication.java
@@ -2,12 +2,14 @@ package com.bank.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class LoanServiceApplication
 {
     public static void main( String[] args )
